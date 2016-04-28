91110f927a8462602e36a6a3ef586321ce707699
danielkermode <danielkermode@hotmail.co.nz>
2016-04-28 18:07:22 +1200

    minify fetch

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-window.__quote__ = '';
-
+var quote = '';
 var errorMessage = 'Oops! There was an error getting the quote.'
 
 function newQuote(e) {
@@ -18,24 +17,25 @@ function newQuote(e) {
   .then(function(data) {
     //call function again if the response quote is the same as the old one.
     //Otherwise change content of quote to the new quote.
-    if(data.quote === window.__quote__) {
+    if(data.quote === quote) {
       newQuote();
     } else {
-      document.getElementById("quote-box").innerText = data.quote;
-      document.getElementById("author-box").innerText = '—' + data.author;
-      document.getElementById("error-div").innerText = '';
-      window.__quote__ = data.quote;
+      document.getElementById('quote-box').innerText = data.quote;
+      document.getElementById('author-box').innerText = '—' + data.author;
+      //reset the error div
+      document.getElementById('error-div').innerText = '';
+      quote = data.quote;
     }
   })
   .catch(function(error) {
     //add the error message to the error-div
-    document.getElementById("error-div").innerText = errorMessage;
+    document.getElementById('error-div').innerText = errorMessage;
   });
 }
 
-window.onload = function() {
-  //get a new quote when the window loads
+document.addEventListener('DOMContentLoaded', function(e) {
+  //get a new quote
   newQuote();
   //assign newQuote to the "new quote" button
-  document.getElementById("quote-btn").onclick = newQuote;
-}
+  document.getElementById('quote-btn').onclick = newQuote;
+});
